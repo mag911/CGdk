35d57019b5664b280e5c03cb5904d9c70316b63c
Rene Hexel <r.hexel@griffith.edu.au>
2018-12-14 09:13:55 +1000

    gdk -> gtk+3 for HomeBrew

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(name: "CGdk",
 	pkgConfig: "gdk-3.0 pangocairo pangoft2 pango gio-unix-2.0 glib-2.0",
 	providers: [
-		.brew(["gdk", "glib", "glib-networking", "gobject-introspection"]),
+		.brew(["gtk+3", "glib", "glib-networking", "gobject-introspection"]),
 		.apt(["libgdk3.0-dev", "glib-networking", "gobject-introspection", "libgirepository1.0-dev"])
 	]
 )
