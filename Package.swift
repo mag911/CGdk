24329e80fffa3ceee906fa378045e99ef3b6b32b
Rene Hexel <r.hexel@griffith.edu.au>
2018-12-14 09:02:35 +1000

    Update to Swift 4 package format

--- Package.swift
+++ Package.swift
@@ -1,3 +1,9 @@
 import PackageDescription 
 
-let package = Package(name: "CGdk")
+let package = Package(name: "CGdk",
+	pkgConfig: "gdk-3.0 pangocairo pangoft2 pango gio-unix-2.0 glib-2.0",
+	providers: [
+		.brew(["gdk", "glib", "glib-networking", "gobject-introspection"]),
+		.apt(["libgdk3.0-dev", "glib-networking", "gobject-introspection", "libgirepository1.0-dev"])
+	]
+)
