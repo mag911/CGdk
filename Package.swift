02db7de0584ccb7a7e7b8b82600f7b9d67792fdf
Rene Hexel <r.hexel@griffith.edu.au>
2019-08-12 09:25:10 +1000

    s/libgdk3.0-dev/libgtk-3-dev/

--- Package.swift
+++ Package.swift
@@ -6,6 +6,6 @@ let package = Package(name: "CGdk",
 	pkgConfig: "gdk-3.0 pangocairo pangoft2 pango gio-unix-2.0 glib-2.0",
 	providers: [
 		.brew(["gtk+3", "glib", "glib-networking", "gobject-introspection"]),
-		.apt(["libgdk3.0-dev", "glib-networking", "gobject-introspection", "libgirepository1.0-dev"])
+		.apt(["libgtk-3-dev", "glib-networking", "gobject-introspection", "libgirepository1.0-dev"])
 	]
 )
